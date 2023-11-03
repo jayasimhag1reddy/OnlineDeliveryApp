39313ecd39fdb1f876e8f5d9aebf64a2640f10af
jayasimhag1reddy <jayasimhag1reddy@gmail.com>
2023-11-03 15:03:48 -0500

    Homepage and Item class updated

--- OnlineDeliveryApp/src/Homepage.java
+++ OnlineDeliveryApp/src/Homepage.java
@@ -1,12 +1,22 @@
 import com.sun.source.tree.WhileLoopTree;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Homepage {
     Scanner sc;
+    Item it;
+    Customer cs;
+    private double cost=0.0;
+    private List<ItemList> order;
     public Homepage(){
         sc=new Scanner(System.in);
+        it=new Item();
+        cs=new Customer();
+        order=new ArrayList<>();
+
     }
     public void display(){
         while(true) {
@@ -20,7 +30,7 @@ public class Homepage {
                     viewMenu();
                 }
                 if(user_choice==2){
-                    placeOrder();
+                    Order();
                 }
                 if(user_choice==3){
                     break;
@@ -34,15 +44,136 @@ public class Homepage {
         }
     }
 
-    private void placeOrder() {
-        
+    private void Order() {
+        viewMenu();
+        while(true) {
+            try {
+                System.out.println("Enter your choice");
+                int choice = sc.nextInt();
+                if(it.hm.containsKey(choice)){
+                    System.out.println("Enter the Quantity");
+                    int qnt=sc.nextInt();
+                    cost+=(qnt*(it.hm.get(choice).getCost()));
+                    order.add(new ItemList(it.hm.get(choice).getName(),qnt,qnt*(it.hm.get(choice).getCost())));
+                }
+                else{
+                    System.out.println("Invalid Choice, Please Enter a valid choice");
+                }
+                System.out.println("Total Cost:"+cost+"$");
+                System.out.println("Do you want to order more, Press Y/y to continue &  Press N/n to Checkout");
+                char ext=sc.next().charAt(0);
+                if(ext=='N' || ext=='n'){
+                    orderCheckout();
+                    break;
+                }
+            }
+            catch (Exception e){
+                System.out.println("Invalid choice");
+                sc.next();
+            }
+        }
+
+
+
     }
 
-    private void viewMenu() {
-        Item it=new Item();
-        for(String s:it.hm.keySet()){
-            System.out.println(it.hm.get(s).getName()+"->"+it.hm.get(s).getCost());
+    private void orderCheckout() {
+        while (true) {
+            try {
+                System.out.println("Enter your choice");
+                System.out.println("Type 1: View Order Details");
+                System.out.println("Type 2: Edit the Order");
+                System.out.println("Type 3: Complete the Order");
+                int order_choice = sc.nextInt();
+                if (order_choice == 1) {
+                    viewOrder();
+                }
+                else if (order_choice == 2) {
+                    editOrder();
+                }
+                else if (order_choice == 3) {
+                    placeOrder();
+                    break;
+                }
+                else if (order_choice < 1 || order_choice > 3) {
+                    System.out.println("Invalid Choice, Please Enter  a valid choice");
+                }
+            }
+            catch (Exception e){
+                System.out.println("Invalid Choice, Please Enter  a valid choice");
+                sc.next();
+            }
         }
     }
 
+    private void placeOrder() {
+
+    }
+
+    private void editOrder() {
+        viewOrder();
+        while(true){
+            try {
+                System.out.println("Enter the Item number you want to edit");
+                int in = sc.nextInt();
+                if (in>=1 && in <= order.size()) {
+                    changeOrder(in);
+                }
+                if (in <= 0 || in > order.size()) {
+                    System.out.println("Invalid Choice, Enter the number within the range of item list");
+                }
+            }
+            catch (Exception e){
+                System.out.println("Invalid Choice, Please Enter Valid Number");
+                sc.next();
+            }
+        }
+
+    }
+
+    private void viewOrder() {
+        System.out.println("Order Details");
+        for(int i=0;i<order.size();i++){
+            System.out.println((i+1)+" "+order.get(i).item+" "+order.get(i).qnt+" "+order.get(i).cost);
+        }
+    }
+
+    private void viewMenu() {
+        for(int s:it.hm.keySet()){
+            System.out.println(it.hm.get(s).getName()+"->"+it.hm.get(s).getCost());
+        }
+    }
+    private void changeOrder(int in){
+        while(true) {
+            System.out.println("Press 'R'/'r' to remove Item from order \n Press 'E'/'e' to edit the Quantity of the item \n Press 'Q'/'q' to Exit ");
+            char ed = sc.next().charAt(0);
+            if (ed == 'R' || ed == 'r') {
+                cost-=order.get(in).cost;
+                order.remove(in);
+            }
+            else if (ed == 'E' || ed == 'e') {
+                System.out.println("Enter the quantity");
+                int q = sc.nextInt();
+                order.get(in).qnt = q;
+                order.get(in).cost = q * (it.hm.get(in).getCost());
+            }
+            else if (ed == 'Q' || ed == 'q') {
+                break;
+            }
+            else{
+                System.out.println("Please Enter a Valid choice");
+            }
+        }
+    }
+
+}
+class ItemList{
+    String item;
+    int qnt;
+    double cost;
+    public ItemList(String item,int qnt,double cost){
+        this.item=item;
+        this.qnt=qnt;
+        this.cost=cost;
+    }
 }
--- OnlineDeliveryApp/src/Item.java
+++ OnlineDeliveryApp/src/Item.java
@@ -5,7 +5,7 @@ public class Item {
     private String name;
     private double cost;
 
-    public HashMap<String,Item> hm;
+    public HashMap<Integer,Item> hm;
     public Item(int id, String name, double cost) {
         this.id = id;
         this.name = name;
@@ -42,22 +42,22 @@ public class Item {
     public void setCost(int cost) {
         this.cost = cost;
     }
-    private void putinMenu(String id,String name,double cost){
+    private void putinMenu(int id,String name,double cost){
         hm.put(id,new Item(name,cost));
     }
     private void Menu(){
-        hm.put("abc123",new Item("Pizza",10.99));
-        hm.put("def456",new Item("Burger",4.99));
-        hm.put("ghi789 ",new Item("Salad",5.49));
-        hm.put("jkl012",new Item("Sushi",12.99));
-        hm.put("mno345",new Item("Pasta ",8.75));
-        hm.put("pqr678",new Item("Steak",19.99));
-        hm.put("stu901",new Item("Ice Cream ",3.49));
-        hm.put("vwx234",new Item("Tacos",6.29));
-        hm.put("yz5678",new Item("Chicken",7.99));
-        hm.put("123abc",new Item("Sandwich",6.99));
-        hm.put("456def",new Item("Soup",4.29));
-        hm.put("789ghi",new Item("Shrimp",15.99));
-        hm.put("012jkl",new Item("Hot Dog",2.99));
+        hm.put(1,new Item("Pizza",10.99));
+        hm.put(2,new Item("Burger",4.99));
+        hm.put(3,new Item("Salad",5.49));
+        hm.put(4,new Item("Sushi",12.99));
+        hm.put(5,new Item("Pasta ",8.75));
+        hm.put(6,new Item("Steak",19.99));
+        hm.put(7,new Item("Ice Cream ",3.49));
+        hm.put(8,new Item("Tacos",6.29));
+        hm.put(9,new Item("Chicken",7.99));
+        hm.put(10,new Item("Sandwich",6.99));
+//        hm.put(11,new Item("Soup",4.29));
+//        hm.put(12,new Item("Shrimp",15.99));
+//        hm.put(13,new Item("Hot Dog",2.99));
     }
 }
